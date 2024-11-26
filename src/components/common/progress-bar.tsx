9abbf3de6b36bed0ae3733b96ef02c6ea363099e
SihyeonHong <hongsihyeon179@gmail.com>
2024-11-26 14:16:48 +0900

    fix: bar className props 추가
    bar의 색깔이 변경될 수 있어서 barClassName 추가했습니다.

--- src/components/common/progress-bar.tsx
+++ src/components/common/progress-bar.tsx
@@ -3,13 +3,15 @@ import { cn } from '~/src/utils/class-name';
 interface ProgressBarProps {
   current: number; // 현재 등록한 인원
   capacity: number; // 최대 수용 인원
-  className?: string;
+  className?: string; // 바깥 컴포넌트
+  barClassName?: string; // 주황색 바 색깔 변경 시
 }
 
 export default function ProgressBar({
   current,
   capacity,
   className,
+  barClassName,
 }: ProgressBarProps) {
   const progressPercentage = Math.min((current / capacity) * 100, 100);
 
@@ -17,7 +19,7 @@ export default function ProgressBar({
     <div className={cn(`h-[60px] w-full px-6 pb-4 pt-2`, className)}>
       <div className="relative h-1 rounded-md bg-white">
         <div
-          className="h-1 rounded-md bg-orange-600"
+          className={cn(`h-1 rounded-md bg-orange-600`, barClassName)}
           style={{ width: `${progressPercentage}%` }}
         ></div>
       </div>
