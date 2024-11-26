155c22764c70f60d001a1939dc2e6ce6e531480e
홍시현 <76465265+SihyeonHong@users.noreply.github.com>
2024-11-26 15:03:38 +0900

    Progress Bar 공통 컴포넌트 구현  (#26)
    * feat: chip 공통 컴포넌트 구현 #6
    
    - chip 컴포넌트의 기본 구조와 스타일링 구현
    - props를 활용한 동적 상태 처리 추가
    
    * chore: @svgr/webpack 설치 및 설정
    
    * feat: 이미지 및 아이콘 추가
    
    * feat: 평점 컴포넌트 생성
    
    * feat: chip 공통 컴포넌트 구현 #6
    
    - chip 컴포넌트의 기본 구조와 스타일링 구현
    - props를 활용한 동적 상태 처리 추가
    
    * feat: chip info, state, time 컴포넌트 구현
    
    chip-info, chip-state, chip-time 컴포넌트 구현
    
    resolves #7, #8, #9
    
    * feat: chip 공통 컴포넌트 구현 #6
    
    - chip 컴포넌트의 기본 구조와 스타일링 구현
    - props를 활용한 동적 상태 처리 추가
    
    * feat: chip info, state, time 컴포넌트 구현
    
    chip-info, chip-state, chip-time 컴포넌트 구현
    
    resolves #7, #8, #9
    
    * feat: badge 컴포넌트 구현
    
    badge 컴포넌트 구현. 999 이상의 숫자는 999+로 표기.
    
    resolves #14
    
    * feat: tag 컴포넌트 구현
    
    resolves #15
    
    * feat: progress bar 공통 컴포넌트 구현
    
    progress bar 공통 컴포넌트 구현
    
    resolves #25
    
    * fix: bar className props 추가
    
    bar의 색깔이 변경될 수 있어서 barClassName 추가했습니다.
    
    ---------
    
    Co-authored-by: devyouth94 <devyouth94@gmail.com>

---
+++ src/components/common/progress-bar.tsx
@@ -0,0 +1,28 @@
+import { cn } from '~/src/utils/class-name';
+
+interface ProgressBarProps {
+  current: number; // 현재 등록한 인원
+  capacity: number; // 최대 수용 인원
+  className?: string; // 바깥 컴포넌트
+  barClassName?: string; // 주황색 바 색깔 변경 시
+}
+
+export default function ProgressBar({
+  current,
+  capacity,
+  className,
+  barClassName,
+}: ProgressBarProps) {
+  const progressPercentage = Math.min((current / capacity) * 100, 100);
+
+  return (
+    <div className={cn(`h-[60px] w-full px-6 pb-4 pt-2`, className)}>
+      <div className="relative h-1 rounded-md bg-white">
+        <div
+          className={cn(`h-1 rounded-md bg-orange-600`, barClassName)}
+          style={{ width: `${progressPercentage}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
