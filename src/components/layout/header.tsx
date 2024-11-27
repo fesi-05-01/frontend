0bb062e28edb76f5e8e0217c7d0e9405c1db033f
yeyiwon <4576874@naver.com>
2024-11-27 14:23:52 +0900

    Revert "fix: gnb 구현"
    This reverts commit 29016a79dccd412fb103f371d435cc177f2d435e.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,21 +1,15 @@
-import Link from 'next/link';
-
-import Gnb from '~/src/components/common/gnb';
 import { cn } from '~/src/utils/class-name';
+
 export default function Header() {
   return (
-    <header className="fixed inset-x-0 top-0 z-10 h-header border-b-2 border-secondary-900 bg-primary-600 text-primary-50">
+    <header className="fixed inset-x-0 top-0 z-10 h-header border-b-2 border-secondary-900 bg-primary-600 font-semibold text-primary-50">
       <section
         className={cn(
           'mx-auto flex h-full max-w-screen-desktop items-center justify-between',
           'px-4 tablet:px-6 desktop:px-0',
         )}
       >
-        <Gnb />
-        <div>
-          {/* <Link href="/mypage">마이페이지</Link> */}
-          <Link href="/login">로그인</Link>
-        </div>
+        {/* 여기에 코드 작성하시면 됩니다~ */}
       </section>
     </header>
   );
