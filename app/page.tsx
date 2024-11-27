0212a165f1b7c77368c4817c4252d16f92956d73
yeyiwon <4576874@naver.com>
2024-11-27 14:25:05 +0900

    Revert "로그인 페이지 구현 레이아웃 문제로 임시 업로드"
    This reverts commit 1a74c89c1073959110b8b17522791ec0483d4918.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link';
-
 import FloatingBar from '~/src/components/layout/floating-bar';
 import MainContainer from '~/src/components/layout/main-container';
 
@@ -7,7 +5,6 @@ export default function Home() {
   return (
     <MainContainer>
       <div>1팀 화이팅~</div>
-      <Link href="/login"> 로그인 </Link>
       <FloatingBar />
     </MainContainer>
   );
