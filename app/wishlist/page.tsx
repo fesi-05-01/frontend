9812abb6a0317bf13f6f0a0d0cb3b6bfeaca1828
devyouth94 <devyouth94@gmail.com>
2024-11-28 18:16:02 +0900

    feat: 찜한 모임 페이지 타이틀 예시 적용

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,3 +1,11 @@
+import PageTitleWithImage from '~/src/components/common/page-title-with-image';
+import MainContainer from '~/src/components/layout/main-container';
+
 export default function WishlistPage() {
-  return <div>WishlistPage</div>;
+  return (
+    <MainContainer>
+      <PageTitleWithImage />
+      WishlistPage
+    </MainContainer>
+  );
 }
