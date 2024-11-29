03963caa460f3ac6caf3196bcedd9892528186e9
김동규 <eastar385@naver.com>
2024-11-29 13:38:12 +0900

    fix: add the className props

--- src/components/common/dropdown.tsx
+++ src/components/common/dropdown.tsx
@@ -2,13 +2,14 @@ import { type ComponentPropsWithoutRef } from 'react';
 
 import { cn } from '~/src/utils/class-name';
 
-interface dropdownProps
+interface DropdownProps
   extends Omit<ComponentPropsWithoutRef<'button'>, 'onClick' | 'onSelect'> {
   onClick?: (option: string) => void;
   options: string[];
   onSelect: (option: string) => void;
   version: 'Login' | 'Filter'; // width의 차이로 편의상 타입명을 지정하였습니다 기능과는 관련이 없습니다
   selectedOption: string;
+  className?: string;
 }
 
 export default function Dropdown({
@@ -16,8 +17,9 @@ export default function Dropdown({
   onSelect,
   version,
   selectedOption,
+  className,
   ...rest
-}: dropdownProps) {
+}: DropdownProps) {
   const handleSelect = (option: string) => {
     onSelect(option);
   };
@@ -27,6 +29,7 @@ export default function Dropdown({
       className={cn(
         'absolute mt-2 rounded-xl bg-white shadow-lg',
         version === 'Filter' ? 'w-full' : 'w-[110px] desktop:w-[142px]',
+        className,
       )}
     >
       {options.map((option) => (
@@ -40,6 +43,7 @@ export default function Dropdown({
               ? 'w-[calc(100%-8px)]'
               : 'h-10 w-[calc(100%-8px)] desktop:h-11',
             selectedOption === option ? 'bg-orange-100' : '',
+            className,
           )}
         >
           {option}
--- src/components/mypage/group-card.tsx
+++ src/components/mypage/group-card.tsx
@@ -2,28 +2,41 @@ import Image from 'next/image';
 
 import Person from '~/src/assets/icons/person.svg?url';
 import NoImage from '~/src/assets/images/bg-login.png';
-import Button from '~/src/components/common/button';
+
 export default function GroupCard() {
+  const isReserved = true;
   return (
-    <div className="h-[308px] w-[311px] border-b-[2px] border-dashed border-secondary-200 mobile:h-[180px] mobile:w-full">
+    <div className="flex h-[308px] w-[311px] border-b-[2px] border-dashed border-secondary-200 mobile:h-[180px] mobile:w-full">
       <Image
         src={NoImage}
         alt="no-image"
         className="h-[156px] w-[311px] rounded-3xl mobile:w-[280px]"
       />
-      <div>
-        <div className="">
-          <span className="">이름</span>
-          <span className="">지역</span>
-        </div>
+      <div className="h-[156px] w-auto flex-col justify-between">
         <div>
-          <span className="">날짜</span>
-          <Image src={Person} alt="person" width={16} height={16} />
-          <span className="">인원수</span>
+          <div className="flex">
+            <span className="">이름</span>
+            <span className="">지역</span>
+          </div>
+          <div className="flex">
+            <span className="">날짜</span>
+            <Image src={Person} alt="person" width={16} height={16} />
+            <span className="">인원수</span>
+          </div>
         </div>
-        <Button className="w-120 h-10 text-sm font-semibold">
-          리뷰 작성하기
-        </Button>
+
+        {isReserved ? (
+          <button
+            className="w-120 h-10 cursor-not-allowed rounded-xl border-[1px] border-orange-600 bg-white px-[22px] py-[10px] text-sm font-semibold text-orange-600"
+            disabled
+          >
+            예약 취소하기
+          </button>
+        ) : (
+          <button className="w-120 h-10 rounded-xl bg-orange-600 px-[22px] py-[10px] text-sm font-semibold text-white">
+            리뷰 작성하기
+          </button>
+        )}
       </div>
     </div>
   );
