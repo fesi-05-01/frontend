66ba4ff56cd0ceddd8fdfbdaefe8f758b1dae182
SihyeonHong <hongsihyeon179@gmail.com>
2024-11-29 16:14:37 +0900

    feat: 모임 페이지 필터, 구분선 추가
    divider 공통 컴포넌트 구현
    모임 페이지에 필터 추가. 내용물은 아직 하드코딩.
    
    resolves #44

--- app/gatherings/page.tsx
+++ app/gatherings/page.tsx
@@ -4,6 +4,8 @@ import { useState } from 'react';
 import useBreakpoint from 'use-breakpoint';
 
 import Button from '~/src/components/common/button';
+import Divider from '~/src/components/common/divider';
+import Filter from '~/src/components/common/filter';
 import GatheringCardLarge from '~/src/components/gathering-card/gathering-card-large';
 import GatheringCardSmall from '~/src/components/gathering-card/gathering-card-small';
 import JoinUsBanner from '~/src/components/gathering-card/join-us-banner';
@@ -20,6 +22,10 @@ const tmpVar: Gathering = {
   participantCount: 5, // 20으로 채우면 cardState 변경
   capacity: 20,
 };
+
+const tmpLocations = ['지역 선택', '건대입구', '을지로3가', '신림', '홍대입구'];
+const tmpDates = ['날짜 선택', '달력 연결 나중에'];
+const tmpFilters = ['마감 임박', '참여 인원 순'];
 
 const BREAKPOINTS = getBreakpoints();
 
@@ -45,16 +51,19 @@ export default function GatheringsPage() {
             <Button className="w-[115px]">모임 만들기</Button>
           </div>
           <div className="flex flex-col gap-4">
-            <div className="c">전체 오피스스트레칭</div>
-            <div className="flex justify-between gap-2">
-              지역전체 닐짜전체 마감임박 하려면 필터랑 드롭다운 클래스네임
-              수정되어야
+            <div className="">전체 오피스스트레칭</div>
+            <Divider />
+            <div className="z-[5] flex justify-between gap-2">
+              <div className="flex gap-2">
+                <Filter version="Right" options={tmpLocations} />
+                <Filter version="Right" options={tmpDates} />
+              </div>
+              <Filter version="Left" options={tmpFilters} />
             </div>
           </div>
         </div>
 
         {/* 카드 */}
-
         {breakpoint === 'tablet' || breakpoint === 'desktop' ? (
           <div className="flex flex-col gap-6">
             <GatheringCardLarge state={tmpState} gathering={tmpVar} />
---
+++ src/components/common/divider.tsx
@@ -0,0 +1,3 @@
+export default function Divider() {
+  return <div className="mx-auto w-[996px] border-t-2 border-gray-200"></div>;
+}
