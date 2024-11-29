d7f697853a75cb4399b5e6d8918bdeee90d0613f
cossgii <163109125+cossgii@users.noreply.github.com>
2024-11-29 13:50:57 +0900

    Mypagecard (#50)
    * feat: add mypage card
    
    * fix: add the className props

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,3 +1,13 @@
+import MainContainer from '~/src/components/layout/main-container';
+import GroupCard from '~/src/components/mypage/group-card';
+
 export default function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <MainContainer>
+      <div>
+        {/* <ProfileCard /> */}
+        <GroupCard />
+      </div>
+    </MainContainer>
+  );
 }
