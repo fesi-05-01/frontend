74f879df904dd97244fac516f5e8a7fa829a7396
김동규 <eastar385@naver.com>
2024-11-29 15:41:17 +0900

    fix: fixing group-card

--- src/components/mypage/group-card.tsx
+++ src/components/mypage/group-card.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image';
 
-import Person from '~/src/assets/icons/person.svg?url';
 import NoImage from '~/src/assets/images/bg-login.png';
+import MemberCountChip from '~/src/components/common/member-count-chip';
+import formatDateTime from '~/src/utils/format-date-time';
 
 export default function GroupCard() {
-  const isReserved = true;
+  const isUsed = true;
+  const dateTime = '2024-11-29T14:30:00.000Z';
+  const { date, time } = formatDateTime(dateTime);
+  const name = '달램핏 오피스 스트레칭';
+  const region = '을지로 3가';
+  const current = 20;
+  const capacity = 20;
+
   return (
     <div className="flex h-[308px] w-[311px] border-b-[2px] border-dashed border-secondary-200 mobile:h-[180px] mobile:w-full">
       <Image
@@ -14,18 +22,25 @@ export default function GroupCard() {
       />
       <div className="h-[156px] w-auto flex-col justify-between">
         <div>
-          <div className="flex">
-            <span className="">이름</span>
-            <span className="">지역</span>
+          <div className="flex gap-2">
+            <span className="text-lg font-semibold text-secondary-900">
+              {name}
+            </span>
+            <span className="text-lg font-semibold text-secondary-900">|</span>
+            <span className="text-sm font-medium text-secondary-700">
+              {region}
+            </span>
           </div>
           <div className="flex">
-            <span className="">날짜</span>
-            <Image src={Person} alt="person" width={16} height={16} />
-            <span className="">인원수</span>
+            <span className="text-sm font-medium text-secondary-700">
+              {date}·{time}
+            </span>
+
+            <MemberCountChip current={current} capacity={capacity} />
           </div>
         </div>
 
-        {isReserved ? (
+        {isUsed ? (
           <button
             className="w-120 h-10 cursor-not-allowed rounded-xl border-[1px] border-orange-600 bg-white px-[22px] py-[10px] text-sm font-semibold text-orange-600"
             disabled
