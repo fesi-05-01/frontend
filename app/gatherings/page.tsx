6c3a725a0a387ec3495b3259f943dc6917cb0ad4
SihyeonHong <hongsihyeon179@gmail.com>
2024-12-02 10:34:06 +0900

    feat: 모임페이지 빈 화면 문구
    모임페이지 빈 화면 문구
    왜 200px을 빼니까 높이가 딱 맞는지는 진짜 모르겠다

--- app/gatherings/page.tsx
+++ app/gatherings/page.tsx
@@ -1,50 +1,47 @@
 'use client';
 
-import { useState } from 'react';
-import useBreakpoint from 'use-breakpoint';
-
+// import useBreakpoint from 'use-breakpoint';
 import Button from '~/src/components/common/button';
 import Divider from '~/src/components/common/divider';
 import Filter from '~/src/components/common/filter';
-import GatheringCardLarge from '~/src/components/gathering-card/gathering-card-large';
-import GatheringCardSmall from '~/src/components/gathering-card/gathering-card-small';
 import JoinUsBanner from '~/src/components/gathering-card/join-us-banner';
 import MainContainer from '~/src/components/layout/main-container';
-import { type Gathering } from '~/src/services/gatherings/types';
-import { getBreakpoints } from '~/src/utils/breakpoints';
+// import { getBreakpoints } from '~/src/utils/breakpoints';
 
-const tmpVar: Gathering = {
-  id: 1,
-  image: '/IMG_1190.jpg',
-  name: '달램핏 마인드풀니스',
-  location: '을지로3가',
-  dateTime: '2024-07-31T09:06:16.184Z',
-  participantCount: 5, // 20으로 채우면 cardState 변경
-  capacity: 20,
-};
+// const tmpVar: Gathering = {
+//   id: 1,
+//   image: '/IMG_1190.jpg',
+//   name: '달램핏 마인드풀니스',
+//   location: '을지로3가',
+//   dateTime: '2024-07-31T09:06:16.184Z',
+//   participantCount: 5, // 20으로 채우면 cardState 변경
+//   capacity: 20,
+// };
 
 const tmpLocations = ['지역 선택', '건대입구', '을지로3가', '신림', '홍대입구'];
 const tmpDates = ['날짜 선택', '달력 연결 나중에'];
 const tmpFilters = ['마감 임박', '참여 인원 순'];
 
-const BREAKPOINTS = getBreakpoints();
+// const BREAKPOINTS = getBreakpoints();
 
 export default function GatheringsPage() {
-  const [tmpState, setTmpState] = useState<'default' | 'disabled'>('default');
-  const { breakpoint } = useBreakpoint(BREAKPOINTS, 'desktop');
+  //   const [tmpState, setTmpState] = useState<'default' | 'disabled'>('default');
+  //   const { breakpoint } = useBreakpoint(BREAKPOINTS, 'desktop');
 
-  const tmpFn = () => {
-    if (tmpState === 'disabled') {
-      setTmpState('default');
-    } else {
-      setTmpState('disabled');
-    }
-  };
+  //   const tmpFn = () => {
+  //     if (tmpState === 'disabled') {
+  //       setTmpState('default');
+  //     } else {
+  //       setTmpState('disabled');
+  //     }
+  //   };
 
   return (
     <MainContainer>
       <JoinUsBanner />
-      <div className="flex flex-col gap-6">
+      {/* 200px을 빼니까 됐는데 왜 되는지 모르겠다 */}
+      <div className="flex h-[calc(100dvh-200px)] flex-col gap-6">
+        {/* 필터링 gap-6 */}
         <div className="flex flex-col gap-2">
           <div className="flex justify-between">
             <div className="">여기 달램핏과 워케이션</div>
@@ -63,8 +60,16 @@ export default function GatheringsPage() {
           </div>
         </div>
 
+        {/* body */}
+        <div className="flex flex-1 items-center justify-center">
+          <div className="text-center text-sm font-medium text-gray-500">
+            아직 모임이 없어요, <br />
+            지금 바로 모임을 만들어보세요!
+          </div>
+        </div>
+
         {/* 카드 */}
-        {breakpoint === 'tablet' || breakpoint === 'desktop' ? (
+        {/* {breakpoint === 'tablet' || breakpoint === 'desktop' ? (
           <div className="flex flex-col gap-6">
             <GatheringCardLarge state={tmpState} gathering={tmpVar} />
           </div>
@@ -72,13 +77,12 @@ export default function GatheringsPage() {
           <div className="flex flex-col items-center gap-6">
             <GatheringCardSmall state={tmpState} gathering={tmpVar} />
           </div>
-        )}
+        )} */}
       </div>
 
-      {breakpoint}
-      <button className="border border-black bg-gray-200 p-2" onClick={tmpFn}>
+      {/* <button className="border border-black bg-gray-200 p-2" onClick={tmpFn}>
         default | disabled
-      </button>
+      </button> */}
     </MainContainer>
   );
 }
