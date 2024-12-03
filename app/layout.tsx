d56a4d5890f426bd4c50d99a4bab890f6994f617
devyouth94 <devyouth94@gmail.com>
2024-12-03 18:22:50 +0900

    feat: msw 프로바이더 적용 및 탄스택 쿼리 프로바이더 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import type { Metadata } from 'next';
 import Header from '~/src/components/layout/header';
 import { pretendard } from '~/src/fonts/fonts';
 import JotaiProvider from '~/src/providers/jotai-provider';
-import { TanstackQueryProvider } from '~/src/providers/tanstack-query-provider';
+import MSWProvider from '~/src/providers/msw-provider';
+import TanstackQueryProvider from '~/src/providers/tanstack-query-provider';
 import { cn } from '~/src/utils/class-name';
 
 import '~/src/styles/globals.css';
@@ -23,12 +24,14 @@ export default function RootLayout({ children }: Readonly<Props>) {
       <body
         className={cn(pretendard.variable, 'bg-secondary-100 font-pretendard')}
       >
-        <JotaiProvider>
-          <TanstackQueryProvider>
-            <Header />
-            {children}
-          </TanstackQueryProvider>
-        </JotaiProvider>
+        <MSWProvider>
+          <JotaiProvider>
+            <TanstackQueryProvider>
+              <Header />
+              {children}
+            </TanstackQueryProvider>
+          </JotaiProvider>
+        </MSWProvider>
       </body>
     </html>
   );
