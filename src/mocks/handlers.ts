ccad0bd6ab41df5659383d5b42aec24d8f972bf6
devyouth94 <devyouth94@gmail.com>
2024-12-04 10:33:29 +0900

    chore: msw handlers array 생성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,9 @@
+import { authsHandlers } from '~/src/mocks/handler/auths';
+import { gatheringsHandlers } from '~/src/mocks/handler/gatherings';
 import { reviewsHandlers } from '~/src/mocks/handler/reviews';
 
-export const handlers = [...reviewsHandlers];
+export const handlers = [
+  ...authsHandlers,
+  ...gatheringsHandlers,
+  ...reviewsHandlers,
+];
