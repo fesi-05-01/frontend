6d85ff4a8e8aa1051aaf6b55ee42a9a27ef7076b
홍시현 <76465265+SihyeonHong@users.noreply.github.com>
2024-12-10 09:26:09 +0900

    찜하기 기능 (#89)
    * feat: 찜한 모임 #87
    
    하트 누르면 로컬스토리지에 저장
    찜한 모임 페이지에 목록 보여주기
    
    * feat: 모임 보내주기
    
    찜했지만 이미 마감된 모임일 경우 보내주기 버튼 누르면 로컬스토리지에서 사라지고 다시 누를 수 없게 버튼 삭제
    
    resolves #87
    
    * feat: 상세페이지 찜버튼

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,11 +1,18 @@
 import PageTitleWithImage from '~/src/components/common/page-title-with-image';
 import MainContainer from '~/src/components/layout/main-container';
+import WishlistContainer from '~/src/components/wishlist/wishlist-container';
+import WishlistPanel from '~/src/components/wishlist/wishlist-panel';
 
 export default function WishlistPage() {
   return (
-    <MainContainer>
+    <MainContainer className="flex flex-col">
       <PageTitleWithImage />
-      WishlistPage
+      <div className="flex flex-1 flex-col gap-6">
+        <WishlistPanel />
+        <div className="flex flex-1 items-center justify-center">
+          <WishlistContainer />
+        </div>
+      </div>
     </MainContainer>
   );
 }
