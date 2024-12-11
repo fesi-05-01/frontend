462d41977748d20e4774bc64c46c5d05bb861933
devyouth94 <devyouth94@gmail.com>
2024-12-11 14:18:17 +0900

    refactor: 모임 탭 수정

--- src/components/common/gathering-tab/sub-tab.tsx
+++ src/components/common/gathering-tab/sub-tab.tsx
@@ -1,13 +1,34 @@
-'use client';
-
-import { useBreakpoint } from 'use-breakpoint';
-
 import Chip from '~/src/components/common/chip';
 import { type GatheringType } from '~/src/services/types';
-import { getBreakpoints } from '~/src/utils/breakpoints';
 import { cn } from '~/src/utils/class-name';
 
-const BREAKPOINT = getBreakpoints();
+interface Props {
+  type: GatheringType;
+  onChangeFilter: (type: GatheringType) => void;
+  className?: string;
+}
+
+export default function Sub({ type, onChangeFilter, className }: Props) {
+  return (
+    <section
+      className={cn(
+        'flex min-h-10 gap-2 transition-opacity duration-300',
+        type === 'WORKATION' && 'opacity-0',
+        className,
+      )}
+    >
+      {VALUE.map(({ label, value }) => (
+        <Chip
+          key={value}
+          state={value === type ? 'active' : 'default'}
+          onClick={() => onChangeFilter(value)}
+        >
+          {label}
+        </Chip>
+      ))}
+    </section>
+  );
+}
 
 const VALUE: Array<{ label: string; value: GatheringType }> = [
   {
@@ -23,34 +44,3 @@ const VALUE: Array<{ label: string; value: GatheringType }> = [
     value: 'MINDFULNESS',
   },
 ];
-
-interface Props {
-  type: GatheringType;
-  onChangeFilter: (type: GatheringType) => void;
-  className?: string;
-}
-
-export default function Sub({ type, onChangeFilter, className }: Props) {
-  const { breakpoint } = useBreakpoint(BREAKPOINT, 'mobile');
-
-  return (
-    <div
-      className={cn(
-        'flex min-h-10 gap-2 transition-opacity duration-300',
-        type === 'WORKATION' && 'opacity-0',
-        className,
-      )}
-    >
-      {VALUE.map(({ label, value }) => (
-        <Chip
-          key={value}
-          size={breakpoint === 'mobile' ? 'small' : 'large'}
-          state={value === type ? 'active' : 'default'}
-          onClick={() => onChangeFilter(value)}
-        >
-          {label}
-        </Chip>
-      ))}
-    </div>
-  );
-}
