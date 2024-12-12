095796bda5198f705dbaf1c3146e146c33a7ee54
홍시현 <76465265+SihyeonHong@users.noreply.github.com>
2024-12-12 18:10:53 +0900

    fix: 모임 목록 페이지 수정 (#121)
    * fix: 모임 목록 페이지 수정
    resolves #113, #114
    
    * 모든 리뷰 페이지 수정 (#120)
    
    * refactor: 정렬 기본값 추가
    
    * style: 모든 리뷰 페이지 css 수정
    리뷰 없을때 안내 문구 패딩 추가
    
    * fix: 모임 목록 페이지 수정
    resolves #113, #114
    
    ---------
    
    Co-authored-by: kimyoungzin <devyouth94@gmail.com>

--- src/components/layout/floating-bar.tsx
+++ src/components/layout/floating-bar.tsx
@@ -32,19 +32,6 @@ export default function FloatingBar({ createdById }: FloatingBarProps) {
         )}
       >
         {isCreator ? (
-          <div className="flex w-full items-center justify-between">
-            <div className="w-[204px] tablet:w-full">
-              <h2 className="font-semibold">더 건강한 나를 위한 프로그램 🏃</h2>
-              <p className="text-xs">
-                국내 최고 웰리스 전문가와 프로그램을 통해 지친 몸과 마음을
-                회복해봐요
-              </p>
-            </div>
-            <Button className="w-[115px]" type="button">
-              참여하기
-            </Button>
-          </div>
-        ) : (
           <div className="flex w-full flex-col gap-2 tablet:flex-row tablet:justify-between">
             <div className="flex flex-col gap-1">
               <h2 className="font-semibold"> 더 건강한 나를 위한 프로그램🏃</h2>
@@ -60,6 +47,19 @@ export default function FloatingBar({ createdById }: FloatingBarProps) {
                 공유하기
               </Button>
             </div>
+          </div>
+        ) : (
+          <div className="flex w-full items-center justify-between">
+            <div className="w-[204px] tablet:w-full">
+              <h2 className="font-semibold">더 건강한 나를 위한 프로그램 🏃</h2>
+              <p className="text-xs">
+                국내 최고 웰리스 전문가와 프로그램을 통해 지친 몸과 마음을
+                회복해봐요
+              </p>
+            </div>
+            <Button className="w-[115px]" type="button">
+              참여하기
+            </Button>
           </div>
         )}
       </section>
