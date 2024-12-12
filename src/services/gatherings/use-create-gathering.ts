112e885ce3577827f7cad01bf99d8a3e08de2793
kimyoungzin <devyouth94@gmail.com>
2024-12-12 15:08:46 +0900

    리뷰 작성, 모임 만들기 로직 수정 (#110)
    * refactor: 모임 만들기 post 요청 이후 쿼리 최신화 로직 추가
    
    * refactor: 리뷰 작성 이후 토스트 메시지 및 쿼리 초기화

--- src/services/gatherings/use-create-gathering.ts
+++ src/services/gatherings/use-create-gathering.ts
@@ -1,11 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
 import { type CreateGatheringForm } from '~/src/components/gatherings/create-gathering-modal/schema';
 import { post } from '~/src/services/api';
 import { type CreateGatheringResponse } from '~/src/services/gatherings/types';
+import { type GatheringType } from '~/src/services/types';
 import { getDateForFormData } from '~/src/utils/date';
 
 export default function useCreateGathering() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (form: CreateGatheringForm) => {
       const { location, type, day, image, capacity } = form;
@@ -21,6 +25,28 @@ export default function useCreateGathering() {
       return post<CreateGatheringResponse>(`/gatherings`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
+        },
+      });
+    },
+    onSuccess: (_, request) => {
+      toast.success('모임이 생성되었습니다.');
+
+      // post 요청 후 쿼리 무효화
+      // 해당하는 타입만 최신화 하도록 작성
+      queryClient.invalidateQueries({
+        predicate: ({ queryKey }) => {
+          if (queryKey[0] !== 'gatherings') return false;
+
+          const { type } = queryKey[1] as { type?: GatheringType };
+
+          if (
+            request.type === 'MINDFULNESS' ||
+            request.type === 'OFFICE_STRETCHING'
+          ) {
+            return type === request.type || type === 'DALLAEMFIT';
+          }
+
+          return type === request.type;
         },
       });
     },
