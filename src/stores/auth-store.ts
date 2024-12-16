4944a3f6754b313feb22274111234e9b62fd32ff
yeyiwon <4576874@naver.com>
2024-12-16 13:31:25 +0900

    feat: 쿠키 수정

--- src/stores/auth-store.ts
+++ src/stores/auth-store.ts
@@ -14,7 +14,7 @@ export const setAccessTokenAtom = atom(
       Cookies.set('accessToken', newToken, {
         secure: true, // HTTPS 에서만
         sameSite: 'strict', // CSRF
-        maxAge: 3600,
+        expires: 1 / 24,
       });
     } else {
       Cookies.remove('accessToken');
@@ -41,7 +41,7 @@ export const setUserInfoAtom = atom(
       Cookies.set('userInfo', JSON.stringify(newUserInfo), {
         secure: true,
         sameSite: 'strict',
-        maxAge: 3600,
+        expires: 1 / 24,
       });
     } else {
       Cookies.remove('userInfo');
