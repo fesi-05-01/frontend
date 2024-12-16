959baf0774c32458346b5622a361c510bdc726f1
devyouth94 <devyouth94@gmail.com>
2024-12-16 15:21:37 +0900

    refactor: 시간 유틸 함수 수정
    분까지 받을수 있도록 수정

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,10 +1,15 @@
 /**
- * 날짜와 시간을 'YYYY-MM-DD' 또는 'YYYY-MM-DDTHH:00:00' 형식의 문자열로 변환합니다.
+ * 날짜와 시간을 'YYYY-MM-DD' 또는 'YYYY-MM-DDTHH:mm:00' 형식의 문자열로 변환합니다.
  * @param {Date} date - 변환할 날짜 객체
- * @param {number} [time] - 시간 (0-23)
+ * @param {number} [hour] - 시간 (0-23)
+ * @param {number} [minute] - 분 (0-59)
  * @returns {string} 날짜/시간 문자열
  * @example
- * // 시간 포함
+ * // 시간과 분 포함
+ * getDateForFormData(new Date('2024-12-05'), 15, 30)
+ * // returns '2024-12-05T15:30:00'
+ *
+ * // 시간만 포함
  * getDateForFormData(new Date('2024-12-05'), 15)
  * // returns '2024-12-05T15:00:00'
  *
@@ -12,15 +17,20 @@
  * getDateForFormData(new Date('2024-12-05'))
  * // returns '2024-12-05'
  */
-export const getDateForFormData = (date: Date, time?: number) => {
+export const getDateForFormData = (
+  date: Date,
+  hour?: number,
+  minute?: number,
+) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
 
-  if (time) {
-    const formattedTime = time.toString().padStart(2, '0');
-    return `${formattedDate}T${formattedTime}:00:00`;
+  if (hour) {
+    const formattedHour = hour.toString().padStart(2, '0');
+    const formattedMinute = (minute || 0).toString().padStart(2, '0');
+    return `${formattedDate}T${formattedHour}:${formattedMinute}:00`;
   }
 
   return formattedDate;
