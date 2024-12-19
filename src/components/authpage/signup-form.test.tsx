57c49815856e883036c7709e476221b875ebdddd
yeyiwon <4576874@naver.com>
2024-12-19 10:22:50 +0900

    feat: auth-test

--- src/components/authpage/signup-form.test.tsx
+++ src/components/authpage/signup-form.test.tsx
@@ -3,13 +3,6 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import SignupForm from '~/src/components/authpage/signup-form';
 import { useSignup } from '~/src/services/auths/use-signup';
 
-// 회원가입 폼 입력 필드 렌더링 테스트 - 통과
-// 모든 필드 유효성 검사 통과 useSignup 호출 테스트 - 통과
-// isPending 로딩중일 때 비활성화 테스트
-// 각 필드마다 빈 값이거나, 잘못된 형식일 때 에러 메시지 표시 테스트 - 통과
-
-jest.mock('~/src/services/auths/use-signup');
-
 jest.mock('~/src/services/auths/use-signup', () => ({
   useSignup: jest.fn(() => ({
     mutate: jest.fn(),
@@ -32,6 +25,13 @@ const getConfirmPasswordInput = () =>
 const getSignupButton = () => screen.getByRole('button', { name: /확인/ });
 
 describe('회원가입 폼', () => {
+  beforeEach(() => {
+    (useSignup as jest.Mock).mockReturnValue({
+      mutate: jest.fn(),
+      isPending: false,
+    });
+  });
+
   test('회원가입 폼 렌더링', () => {
     renderSignupForm();
 
@@ -44,227 +44,234 @@ describe('회원가입 폼', () => {
     expect(getSignupButton()).toBeInTheDocument();
   });
 
-  test('모든 필드 유효성 통과 useSignup 호출 테스트', async () => {
-    const mockMutate = jest.fn();
-    const mockUseSignup = jest.fn(() => ({
-      mutate: mockMutate,
-      isPending: false,
-    }));
+  describe('유효성 검사 실패', () => {
+    // 이름
+    test('이름 입력 테스트', async () => {
+      renderSignupForm();
 
-    (useSignup as jest.Mock).mockReturnValue(mockUseSignup());
+      const nameInput = getNameInput();
+      fireEvent.change(nameInput, { target: { value: '' } });
 
-    renderSignupForm();
-    const nameInput = getNameInput();
-    const emailInput = getEmailInput();
-    const companyNameInput = getCompanyNameInput();
-    const passwordInput = getPasswordInput();
-    const confirmPasswordInput = getConfirmPasswordInput();
-    const signupButton = getSignupButton();
+      const signupButton = getSignupButton();
+      fireEvent.submit(signupButton);
 
-    fireEvent.change(nameInput, { target: { value: '이름' } });
-    fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
-    fireEvent.change(companyNameInput, { target: { value: '회사' } });
-    fireEvent.change(passwordInput, { target: { value: 'test1234*' } });
-    fireEvent.change(confirmPasswordInput, { target: { value: 'test1234*' } });
-    fireEvent.submit(signupButton);
+      await waitFor(() => {
+        expect(screen.queryByText(/이름을 입력해주세요/)).toBeInTheDocument();
+      });
+    });
 
-    await waitFor(() => {
-      expect(mockMutate).toHaveBeenCalledWith({
-        name: '이름',
-        email: 'test@test.com',
-        companyName: '회사',
-        password: 'test1234*',
-        confirmPassword: 'test1234*',
+    test('이름 형식 테스트', async () => {
+      renderSignupForm();
+
+      const nameInput = getNameInput();
+      fireEvent.change(nameInput, { target: { value: '가' } });
+
+      const signupButton = getSignupButton();
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(
+          screen.queryByText(/이름은 2글자 이상이어야 합니다/),
+        ).toBeInTheDocument();
+      });
+    });
+
+    // 이메일
+    test('이메일 입력 테스트', async () => {
+      renderSignupForm();
+
+      const emailInput = getEmailInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(emailInput, { target: { value: '' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(screen.getByText(/이메일을 입력해주세요/)).toBeInTheDocument();
+      });
+    });
+
+    test('잘못된 이메일 형식 테스트', async () => {
+      renderSignupForm();
+
+      const emailInput = getEmailInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(screen.getByText(/이메일 형식이 아닙니다/)).toBeInTheDocument();
+      });
+    });
+
+    // 회사
+    test('회사 입력 테스트', async () => {
+      renderSignupForm();
+
+      const companyNameInput = getCompanyNameInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(companyNameInput, { target: { value: '' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(screen.getByText(/회사명을 입력해주세요/)).toBeInTheDocument();
+      });
+    });
+
+    test('회사 입력 테스트', async () => {
+      renderSignupForm();
+
+      const companyNameInput = getCompanyNameInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(companyNameInput, { target: { value: '화' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/회사명을 정확히 입력해주세요/),
+        ).toBeInTheDocument();
+      });
+    });
+
+    // 비밀번호
+    test('비밀번호 입력 테스트', async () => {
+      renderSignupForm();
+
+      const passwordInput = getPasswordInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(passwordInput, { target: { value: '' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(screen.getByText(/비밀번호를 입력해주세요/)).toBeInTheDocument();
+      });
+    });
+
+    test('비밀번호 8자 이상 입력 안내 테스트', async () => {
+      renderSignupForm();
+
+      const passwordInput = getPasswordInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(passwordInput, { target: { value: 'test' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/비밀번호는 8자리 이상이어야 합니다/),
+        ).toBeInTheDocument();
+      });
+    });
+
+    test('비밀번호 형식 테스트', async () => {
+      renderSignupForm();
+
+      const passwordInput = getPasswordInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(passwordInput, { target: { value: 'test1234' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(
+            /영문, 숫자, 특수문자\(~!@#\$%\^&\*\)를 모두 조합해 주세요/,
+          ),
+        ).toBeInTheDocument();
+      });
+    });
+
+    // 비밀번호 확인
+    test('비밀번호 확인 입력 테스트', async () => {
+      renderSignupForm();
+
+      const confirmPasswordInput = getConfirmPasswordInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(confirmPasswordInput, { target: { value: '' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/비밀번호를 다시 입력해주세요/),
+        ).toBeInTheDocument();
+      });
+    });
+
+    test('비밀번호 확인이 일치하지 않을 경우 ', async () => {
+      renderSignupForm();
+
+      const passwordInput = getPasswordInput();
+      const confirmPasswordInput = getConfirmPasswordInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(passwordInput, { target: { value: 'test1234*' } });
+      fireEvent.change(confirmPasswordInput, { target: { value: 'test1234' } });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(
+          screen.getByText(/비밀번호가 일치하지 않습니다/),
+        ).toBeInTheDocument();
       });
     });
   });
+  describe('모든 필드 검사 통과', () => {
+    test('모든 필드 유효성 통과 useSignup 호출 테스트', async () => {
+      const mockMutate = jest.fn();
 
-  test('isPending 테스트', async () => {
-    const mockUseSignup = jest.fn().mockReturnValue({
-      mutate: jest.fn(),
-      isPending: true,
+      (useSignup as jest.Mock).mockReturnValue({
+        mutate: mockMutate,
+        isPending: false,
+      });
+
+      renderSignupForm();
+      const nameInput = getNameInput();
+      const emailInput = getEmailInput();
+      const companyNameInput = getCompanyNameInput();
+      const passwordInput = getPasswordInput();
+      const confirmPasswordInput = getConfirmPasswordInput();
+      const signupButton = getSignupButton();
+
+      fireEvent.change(nameInput, { target: { value: '이름' } });
+      fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
+      fireEvent.change(companyNameInput, { target: { value: '회사' } });
+      fireEvent.change(passwordInput, { target: { value: 'test1234*' } });
+      fireEvent.change(confirmPasswordInput, {
+        target: { value: 'test1234*' },
+      });
+      fireEvent.submit(signupButton);
+
+      await waitFor(() => {
+        expect(mockMutate).toHaveBeenCalledWith({
+          name: '이름',
+          email: 'test@test.com',
+          companyName: '회사',
+          password: 'test1234*',
+          confirmPassword: 'test1234*',
+        });
+      });
     });
 
-    (useSignup as jest.Mock).mockReturnValue(mockUseSignup());
+    test('isPending 테스트', async () => {
+      (useSignup as jest.Mock).mockReturnValue({
+        mutate: jest.fn(),
+        isPending: true,
+      });
 
-    renderSignupForm();
+      renderSignupForm();
 
-    const signupButton = getSignupButton();
+      const signupButton = getSignupButton();
 
-    fireEvent.submit(signupButton);
+      fireEvent.submit(signupButton);
 
-    await waitFor(() => {
-      expect(signupButton).toBeDisabled();
-    });
-  });
-
-  // 이름
-  test('이름 입력 테스트', async () => {
-    renderSignupForm();
-
-    const nameInput = getNameInput();
-    fireEvent.change(nameInput, { target: { value: '' } });
-
-    const signupButton = getSignupButton();
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(screen.queryByText(/이름을 입력해주세요/)).toBeInTheDocument();
-    });
-  });
-  test('이름 형식 테스트', async () => {
-    renderSignupForm();
-
-    const nameInput = getNameInput();
-    fireEvent.change(nameInput, { target: { value: '가' } });
-
-    const signupButton = getSignupButton();
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(
-        screen.queryByText(/이름은 2글자 이상이어야 합니다/),
-      ).toBeInTheDocument();
-    });
-  });
-
-  // 이메일
-  test('이메일 입력 테스트', async () => {
-    renderSignupForm();
-
-    const emailInput = getEmailInput();
-    const signupButton = getSignupButton();
-
-    // 잘못된 이메일 형식 입력
-    fireEvent.change(emailInput, { target: { value: '' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(screen.getByText(/이메일을 입력해주세요/)).toBeInTheDocument();
-    });
-  });
-  test('잘못된 이메일 형식 테스트', async () => {
-    renderSignupForm();
-
-    const emailInput = getEmailInput();
-    const signupButton = getSignupButton();
-
-    // 잘못된 이메일 형식 입력
-    fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(screen.getByText(/이메일 형식이 아닙니다/)).toBeInTheDocument();
-    });
-  });
-  // 회사
-  test('회사 입력 테스트', async () => {
-    renderSignupForm();
-
-    const companyNameInput = getCompanyNameInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(companyNameInput, { target: { value: '' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(screen.getByText(/회사명을 입력해주세요/)).toBeInTheDocument();
-    });
-  });
-  test('회사 입력 테스트', async () => {
-    renderSignupForm();
-
-    const companyNameInput = getCompanyNameInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(companyNameInput, { target: { value: '화' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(
-        screen.getByText(/회사명을 정확히 입력해주세요/),
-      ).toBeInTheDocument();
-    });
-  });
-  // 비밀번호
-
-  test('비밀번호 입력 테스트', async () => {
-    renderSignupForm();
-
-    const passwordInput = getPasswordInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(passwordInput, { target: { value: '' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(screen.getByText(/비밀번호를 입력해주세요/)).toBeInTheDocument();
-    });
-  });
-  test('비밀번호 8자 이상 입력 안내 테스트', async () => {
-    renderSignupForm();
-
-    const passwordInput = getPasswordInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(passwordInput, { target: { value: 'test' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(
-        screen.getByText(/비밀번호는 8자리 이상이어야 합니다/),
-      ).toBeInTheDocument();
-    });
-  });
-  test('비밀번호 형식 테스트', async () => {
-    renderSignupForm();
-
-    const passwordInput = getPasswordInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(passwordInput, { target: { value: 'test1234' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(
-        screen.getByText(
-          /영문, 숫자, 특수문자\(~!@#\$%\^&\*\)를 모두 조합해 주세요/,
-        ),
-      ).toBeInTheDocument();
-    });
-  });
-
-  // 비밀번호 확인
-  test('비밀번호 확인 입력 테스트', async () => {
-    renderSignupForm();
-
-    const confirmPasswordInput = getConfirmPasswordInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(confirmPasswordInput, { target: { value: '' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(
-        screen.getByText(/비밀번호를 다시 입력해주세요/),
-      ).toBeInTheDocument();
-    });
-  });
-  test('비밀번호 확인이 일치하지 않을 경우 ', async () => {
-    renderSignupForm();
-
-    const passwordInput = getPasswordInput();
-    const confirmPasswordInput = getConfirmPasswordInput();
-    const signupButton = getSignupButton();
-
-    fireEvent.change(passwordInput, { target: { value: 'test1234*' } });
-    fireEvent.change(confirmPasswordInput, { target: { value: 'test1234' } });
-    fireEvent.submit(signupButton);
-
-    await waitFor(() => {
-      expect(
-        screen.getByText(/비밀번호가 일치하지 않습니다/),
-      ).toBeInTheDocument();
+      await waitFor(() => {
+        expect(signupButton).toBeDisabled();
+      });
     });
   });
 });
--- src/components/authpage/login-form.test.tsx
+++ src/components/authpage/login-form.test.tsx
@@ -9,13 +9,8 @@ import { useLogin } from '~/src/services/auths/use-login';
 // useLogin 호출 테스트.
 // isPending 테스트
 
-jest.mock('~/src/services/auths/use-login');
-
 jest.mock('~/src/services/auths/use-login', () => ({
-  useLogin: jest.fn(() => ({
-    mutate: jest.fn(),
-    isPending: false,
-  })),
+  useLogin: jest.fn(),
 }));
 
 const mockData = {
@@ -32,6 +27,13 @@ const getPasswordInput = () =>
 const getLoginButton = () => screen.getByRole('button', { name: /로그인/ });
 
 describe('로그인 폼', () => {
+  beforeEach(() => {
+    (useLogin as jest.Mock).mockReturnValue({
+      mutate: jest.fn(),
+      isPending: false,
+    });
+  });
+
   test('아이디 비번 입력 필드 렌더링', () => {
     renderLoginForm();
 
@@ -40,76 +42,81 @@ describe('로그인 폼', () => {
     expect(getLoginButton()).toBeInTheDocument();
   });
 
-  test('빈 입력 필드에 대해 에러 메시지가 표시되는지 테스트', async () => {
-    renderLoginForm();
+  describe('유효성 검사', () => {
+    test('빈 입력 필드에 대해 에러 메시지가 표시되는지 테스트', async () => {
+      renderLoginForm();
 
-    fireEvent.submit(getLoginButton());
+      fireEvent.submit(getLoginButton());
 
-    // 에러 메시지가 렌더링되는지 확인
-    await waitFor(() => {
-      expect(screen.getByText(/이메일을 입력해주세요/)).toBeInTheDocument();
-      expect(screen.getByText(/비밀번호를 입력해주세요/)).toBeInTheDocument();
+      // 에러 메시지가 렌더링되는지 확인
+      await waitFor(() => {
+        expect(screen.getByText(/이메일을 입력해주세요/)).toBeInTheDocument();
+        expect(screen.getByText(/비밀번호를 입력해주세요/)).toBeInTheDocument();
+      });
     });
-  });
 
-  test('잘못된 이메일 형식에 대해 에러 메시지가 표시되는지 테스트', async () => {
-    renderLoginForm();
+    test('잘못된 이메일 형식에 대해 에러 메시지가 표시되는지 테스트', async () => {
+      renderLoginForm();
 
-    const emailInput = getEmailInput();
-    const passwordInput = getPasswordInput();
-    const loginButton = getLoginButton();
+      const emailInput = getEmailInput();
+      const passwordInput = getPasswordInput();
+      const loginButton = getLoginButton();
 
-    // 잘못된 이메일 형식 입력
-    fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
-    fireEvent.change(passwordInput, { target: { value: mockData.password } });
-    fireEvent.submit(loginButton);
+      // 잘못된 이메일 형식 입력
+      fireEvent.change(emailInput, { target: { value: 'invalid-email' } });
+      fireEvent.change(passwordInput, { target: { value: mockData.password } });
+      fireEvent.submit(loginButton);
 
-    await waitFor(() => {
-      expect(screen.getByText(/이메일 형식이 아닙니다/)).toBeInTheDocument();
-    });
-  });
-  test('유효성 검사 통과 useLogin 호출 테스트', async () => {
-    const mockMutate = jest.fn();
-    const mockUseLogin = jest.fn(() => ({
-      mutate: mockMutate,
-      isPending: false,
-    }));
-
-    (useLogin as jest.Mock).mockReturnValue(mockUseLogin());
-
-    renderLoginForm();
-
-    const emailInput = getEmailInput();
-    const passwordInput = getPasswordInput();
-    const loginButton = getLoginButton();
-
-    fireEvent.change(emailInput, { target: { value: mockData.email } });
-    fireEvent.change(passwordInput, { target: { value: mockData.password } });
-    fireEvent.submit(loginButton);
-
-    await waitFor(() => {
-      expect(mockMutate).toHaveBeenCalledWith({
-        email: mockData.email,
-        password: mockData.password,
+      await waitFor(() => {
+        expect(screen.getByText(/이메일 형식이 아닙니다/)).toBeInTheDocument();
       });
     });
   });
-  test('isPending 테스트', async () => {
-    const mockUseLogin = jest.fn().mockReturnValue({
-      mutate: jest.fn(),
-      isPending: true,
+
+  describe('모든 필드가 유효한 경우 테스트', () => {
+    test('유효성 검사 통과 useLogin 호출 테스트', async () => {
+      const mockMutate = jest.fn();
+      const mockUseLogin = jest.fn(() => ({
+        mutate: mockMutate,
+        isPending: false,
+      }));
+
+      (useLogin as jest.Mock).mockReturnValue(mockUseLogin());
+
+      renderLoginForm();
+
+      const emailInput = getEmailInput();
+      const passwordInput = getPasswordInput();
+      const loginButton = getLoginButton();
+
+      fireEvent.change(emailInput, { target: { value: mockData.email } });
+      fireEvent.change(passwordInput, { target: { value: mockData.password } });
+      fireEvent.submit(loginButton);
+
+      await waitFor(() => {
+        expect(mockMutate).toHaveBeenCalledWith({
+          email: mockData.email,
+          password: mockData.password,
+        });
+      });
     });
+    test('isPending 테스트', async () => {
+      const mockUseLogin = jest.fn().mockReturnValue({
+        mutate: jest.fn(),
+        isPending: true,
+      });
 
-    (useLogin as jest.Mock).mockReturnValue(mockUseLogin());
+      (useLogin as jest.Mock).mockReturnValue(mockUseLogin());
 
-    renderLoginForm();
+      renderLoginForm();
 
-    const loginButton = getLoginButton();
+      const loginButton = getLoginButton();
 
-    fireEvent.submit(loginButton);
+      fireEvent.submit(loginButton);
 
-    await waitFor(() => {
-      expect(loginButton).toBeDisabled();
+      await waitFor(() => {
+        expect(loginButton).toBeDisabled();
+      });
     });
   });
 });
