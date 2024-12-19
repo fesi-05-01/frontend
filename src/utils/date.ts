d2cf34ccf255d4b63fcf652471f7c3bfbf5791ff
devyouth94 <devyouth94@gmail.com>
2024-12-19 14:36:44 +0900

    test: getDateForFormData 테스트 코드 추가

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -22,12 +22,26 @@ export const getDateForFormData = (
   hour?: number,
   minute?: number,
 ) => {
+  // 시간 유효성 검사
+  if (hour !== undefined) {
+    if (hour < 0 || hour >= 24) {
+      throw new Error('시간은 0-23 사이의 값이어야 합니다.');
+    }
+  }
+
+  // 분 유효성 검사
+  if (minute !== undefined) {
+    if (minute < 0 || minute >= 60) {
+      throw new Error('분은 0-59 사이의 값이어야 합니다.');
+    }
+  }
+
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   const formattedDate = `${year}-${month}-${day}`;
 
-  if (hour) {
+  if (hour !== undefined) {
     const formattedHour = hour.toString().padStart(2, '0');
     const formattedMinute = (minute || 0).toString().padStart(2, '0');
     return `${formattedDate}T${formattedHour}:${formattedMinute}:00`;
