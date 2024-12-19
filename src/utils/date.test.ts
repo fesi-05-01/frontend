e9d4814e40e654e3c62cd98bb0377fbbe0460012
devyouth94 <devyouth94@gmail.com>
2024-12-19 15:20:32 +0900

    test: convertTo24Hour 테스트코드 추가

--- src/utils/date.test.ts
+++ src/utils/date.test.ts
@@ -1,4 +1,4 @@
-import { getDateForFormData } from '~/src/utils/date';
+import { convertTo24Hour, getDateForFormData } from '~/src/utils/date';
 
 describe('getDateForFormData', () => {
   describe('성공 케이스', () => {
@@ -49,6 +49,38 @@ describe('getDateForFormData', () => {
     it('12시 0분이 정상적으로 처리되어야 함', () => {
       const testDate = new Date('2024-12-19');
       expect(getDateForFormData(testDate, 12, 0)).toBe('2024-12-19T12:00:00');
+    });
+  });
+});
+
+describe('convertTo24Hour', () => {
+  describe('성공 케이스', () => {
+    it('AM 9시는 9시로 변환되어야 함', () => {
+      expect(convertTo24Hour({ hour: 9, ampm: 'AM' })).toBe(9);
+    });
+
+    it('PM 12시는 12시로 변환되어야 함', () => {
+      expect(convertTo24Hour({ hour: 12, ampm: 'PM' })).toBe(12);
+    });
+
+    it('PM 9시는 21시로 변환되어야 함', () => {
+      expect(convertTo24Hour({ hour: 9, ampm: 'PM' })).toBe(21);
+    });
+  });
+
+  describe('실패 케이스', () => {
+    it('시간은 1 미만이 될 수 없음', () => {
+      expect(() => convertTo24Hour({ hour: 0, ampm: 'AM' })).toThrow();
+    });
+
+    it('시간은 12 초과가 될 수 없음', () => {
+      expect(() => convertTo24Hour({ hour: 13, ampm: 'PM' })).toThrow();
+    });
+  });
+
+  describe('경계값 케이스', () => {
+    it('AM 12시는 0시로 변환되어야 함', () => {
+      expect(convertTo24Hour({ hour: 12, ampm: 'AM' })).toBe(0);
     });
   });
 });
