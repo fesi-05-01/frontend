ae23a75fcbd0b85d6d7469c513c0948dce2948e4
yeyiwon <4576874@naver.com>
2024-12-23 09:29:00 +0900

    feat: test 코드 수정

--- src/components/authpage/validation/auth-schema.test.tsx
+++ src/components/authpage/validation/auth-schema.test.tsx
@@ -13,14 +13,14 @@ describe('signupSchema', () => {
   it('잘못된 이메일일 경우', () => {
     const invalidEmail = { ...validData, email: 'invalidEmail' };
     expect(() => signupSchema.parse(invalidEmail)).toThrow(
-      '이메일 형식이 아닙니다.',
+      '이메일 형식이 아닙니다',
     );
   });
 
   it('이름이 너무 짧을 경우', () => {
     const invalidName = { ...validData, name: 'a' };
     expect(() => signupSchema.parse(invalidName)).toThrow(
-      '이름은 2글자 이상이어야 합니다.',
+      '이름은 2글자 이상이어야 합니다',
     );
   });
 
@@ -34,14 +34,14 @@ describe('signupSchema', () => {
   it('비밀번호가 너무 짧을 경우', () => {
     const shortPasswordData = { ...validData, password: '1234' };
     expect(() => signupSchema.parse(shortPasswordData)).toThrow(
-      '비밀번호는 8자리 이상이어야 합니다.',
+      '비밀번호는 8자리 이상이어야 합니다',
     );
   });
 
   it('비밀번호에 영문, 숫자, 특수문자가 모두 포함되지 않을 경우', () => {
     const invalidPasswordData = { ...validData, password: 'test1234' };
     expect(() => signupSchema.parse(invalidPasswordData)).toThrow(
-      '영문, 숫자, 특수문자(~!@#$%^&*)를 모두 조합해 주세요.',
+      '영문, 숫자, 특수문자(~!@#$%^&*)를 모두 조합해 주세요',
     );
   });
 
@@ -52,7 +52,7 @@ describe('signupSchema', () => {
       confirmPassword: 'DifferentPassword',
     };
     expect(() => signupSchema.parse(differentPasswordData)).toThrow(
-      '비밀번호가 일치하지 않습니다.',
+      '비밀번호가 일치하지 않습니다',
     );
   });
 
@@ -71,7 +71,7 @@ describe('loginSchema', () => {
   it('잘못된 이메일일 경우', () => {
     const invalidEmail = { ...validData, email: 'invalidEmail' };
     expect(() => loginSchema.parse(invalidEmail)).toThrow(
-      '이메일 형식이 아닙니다.',
+      '이메일 형식이 아닙니다',
     );
   });
 
